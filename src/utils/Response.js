62a2f0775a57a075dced45f56fd0108132eb593f
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-28 14:09:38 -0300

    refactor Response class for improved readability and consistency

--- src/utils/Response.js
+++ src/utils/Response.js
@@ -1,24 +1,23 @@
 export class Response {
-    /**
-     * Response object
-     * @param {number} status
-     * @param {string} message
-     * @param {object | string} data
-     */
+	/**
+	 * Response object
+	 * @param {number} status
+	 * @param {string} message
+	 * @param {object | string} data
+	 */
 
-    constructor(status, message, data = null) {
-      this.status = status;
-      this.message = message;
-      this.data = data;
-      this.error = null;
-    }
+	constructor(status, message, data = null) {
+		this.status = status;
+		this.message = message;
+		this.data = data;
+		this.error = null;
+	}
 
-    /**
-     * Set a error inside the response
-     * 
-     * @param {object} errorObject 
-     */
-    setError(errorObject) {
-      this.error = errorObject;
-    }
-  }
+	/**
+	 * Set a error inside the response
+	 * @param {Error} errorObject
+	 */
+	setError(errorObject) {
+		this.error = errorObject;
+	}
+}

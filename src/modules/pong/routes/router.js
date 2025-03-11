1cca0e825173fbd2caf70ed947fa5e5752153a63
AdadaEdu <eduardo_adada@hotmail.com>
2025-03-11 15:23:42 -0300

    initial config

--- src/modules/pong/routes/router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { PongController } from '../controller/PongController.js';
-
-const routes = Router();
-const pongController = new PongController();
-
-routes.get('/', pongController.getPong);
-
-export { routes as pongRoutes };
--- src/server.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js';
-
-// aaaa
-app.listen(process.env.PORT || 3333, () => {
-	console.log(
-		`\n\nBack-end Online on ` + `http://localhost:${process.env.PORT}` ||
-			'3333' + '🚀🚀🚀',
-	);
-});

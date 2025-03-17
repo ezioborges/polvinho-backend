a71a7fe5aea6c1c166e4ae598f07422609eda256
ezioborges <eziopborges@gmail.com>
2025-03-17 10:09:41 -0300

    update: app.js to include express.json and urlencoded

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import professor from './routes/professorRouter.js'
 export const app = express()
 export const port = 3333
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost:27017').then(() => {
   console.log('Mongo conectado com sucesso! :)');
--- src/routes/professorRouter.js
+++ src/routes/professorRouter.js
@@ -1,10 +1,37 @@
 import express from 'express'
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
+import '../modules/Professor/model/ProfessorSchema.js'
+
+const Professor = mongoose.model('professors')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  return res.status(201).send({ message: 'Aqui ta chegando' })
+router.post('/', (req, res) => {
+  const newProfessor = {
+    name: req.body.name,
+    email: req.body.email,
+    registration: req.body.registration, 
+    passwordHash: req.body.passwordHash,
+    role: req.body.role,
+    createdAt: req.body.createdAt,
+    updatedAt: null,
+    isDeleted: req.body.isDeleted
+  }
+  console.log(newProfessor);
+  
+  new Professor(newProfessor).save().then(() => {
+    console.log('Professor cadastrado com sucesso!');
+  }).catch(err => {
+    console.log(`Erro ao cadastrar professor: ${err}`);
+  })
+
+  return res.status(201).send()
+})
+
+router.get('/', async (req, res) => {
+  const professors = await Professor.find()
+
+  res.status(200).send({ professors })
 })
 
 export default router;

783a8e917d0170423683e1c440668d2adb8c230c
ezioborges <eziopborges@gmail.com>
2025-03-17 09:16:29 -0300

    config: directories, conection express and mongoose

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import professor from './routes/professorRouter.js'
 
 export const app = express()
 export const port = 3333
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost:27017').then(() => {
+  console.log('Mongo conectado com sucesso! :)');
+}).catch(err => {
+  console.log(`Erro ao conectar com o mongodb: ${err}`);
+})
+
+app.use('/professor', professor)

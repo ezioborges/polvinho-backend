75f9179263a91f26fe9afd6fc5d07f18463e6fc4
Gustavo <gustavoleandrogorges@gmail.com>
2025-03-17 12:47:53 -0300

    remove database config com app and place it on its own file

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import professor from './routes/professorRouter.js'
+import express from 'express';
+import './database.js';
+import professor from './routes/professorRouter.js';
 
-export const app = express()
-export const port = 3333
+export const app = express();
+export const port = 3333;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017').then(() => {
-  console.log('Mongo conectado com sucesso! :)');
-}).catch(err => {
-  console.log(`Erro ao conectar com o mongodb: ${err}`);
-})
-
-app.use('/professor', professor)
+app.use('/professor', professor);

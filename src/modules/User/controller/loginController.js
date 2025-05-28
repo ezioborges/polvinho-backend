ae32b778c9a6e474eec63a270fb4807f3bbed4e7
ezioborges <eziopborges@gmail.com>
2025-05-28 10:51:40 -0300

    refactor: reorganize imports and improve login error messages

--- src/modules/User/controller/loginController.js
+++ src/modules/User/controller/loginController.js
@@ -1,54 +1,59 @@
-import jwt from 'jsonwebtoken';
-import User from '../model/UserSchema.js';
-import { env } from '../../../env/index.js'; 
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { env } from '../../../env/index.js';
+import User from '../model/UserSchema.js';
 
 const JWT_SECRET = env.JWT_SECRET;
 
 const login = async (req, res) => {
-    try {
-        const { email, password, registration } = req.body;
+	try {
+		const { email, password, registration } = req.body;
 
-        let user;  
+		let user;
 
-        if (email) {
-            user = await User.findOne({ email })
-        } else if (registration) {
-            user = await User.findOne({ registration })
-        }
+		if (email) {
+			user = await User.findOne({ email });
+		} else if (registration) {
+			user = await User.findOne({ registration });
+		}
 
-        if (!user) {
-            return res.status(404).send({ message: "Credenciais inválidas" })
-        };
+		if (!user) {
+			return res
+				.status(404)
+				.send({ message: 'Email ou matrícula inválidos' });
+		}
 
-        const isPasswordValid = await bcrypt.compare(password, user.passwordHash)
+		const isPasswordValid = await bcrypt.compare(
+			password,
+			user.passwordHash,
+		);
 
-        if (!isPasswordValid) {
-            return res.status(401).send({ message: "Senha inválida" })
-        }
+		if (!isPasswordValid) {
+			return res.status(401).send({ message: 'Senha inválida' });
+		}
 
-        const token = jwt.sign(
-            { id: user._id, email: user.email},
-            JWT_SECRET,
-            { expiresIn: '1h' }
-        )
+		const token = jwt.sign(
+			{ id: user._id, email: user.email },
+			JWT_SECRET,
+			{ expiresIn: '1h' },
+		);
 
-        return res.status(200).send({
-            message: "Login realizado com sucesso",
-            token,
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email,
-                role: user.role
-            }
-        })
-    } catch (error) {
-        return res.status(500).send({
-            message: "Erro ao realizar login",
-            error: error.message
-        });        
-    }
-}
+		return res.status(200).send({
+			message: 'Login realizado com sucesso',
+			token,
+			user: {
+				id: user._id,
+				name: user.name,
+				email: user.email,
+				role: user.role,
+			},
+		});
+	} catch (error) {
+		return res.status(500).send({
+			message: 'Erro ao realizar login',
+			error: error.message,
+		});
+	}
+};
 
 export default login;

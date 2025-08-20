2bb6f0cfff0b9a00ab9ebb1ba89398968517c4f0
ezioborges <eziopborges@gmail.com>
2025-08-20 16:56:10 -0300

    fix: configure CORS middleware with specific options for enhanced security

--- src/app.js
+++ src/app.js
@@ -13,7 +13,13 @@ export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+	cors({
+		origin: 'https://little-octopus.vercel.app',
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+	}),
+);
 
 app.use('/login', login);
 app.use('/subjects', subjects);

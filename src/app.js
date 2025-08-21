04913005f8bdaea2ceb85f053adeb3d324abbaba
Ezio Borges <eziopborges@gmail.com>
2025-08-21 10:46:02 -0300

    fix: simplify CORS middleware configuration by removing specific options

--- src/app.js
+++ src/app.js
@@ -13,13 +13,7 @@ export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-	cors({
-		origin: 'https://little-octopus.vercel.app',
-		methods: ['GET', 'POST', 'PUT', 'DELETE'],
-		allowedHeaders: ['Content-Type', 'Authorization'],
-	}),
-);
+app.use(cors());
 
 app.use('/login', login);
 app.use('/subjects', subjects);
